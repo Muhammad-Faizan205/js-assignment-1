839d6c4fbf79153ece187e0aacecdb3a0472a069
muhammad faizan <smartfaiza53@gmail.com>
2023-09-13 16:26:28 +0500

    work complete

---
+++ js assignment1/app.js
@@ -0,0 +1,191 @@
+// --------------- Chapter 1 (Alerts)
+
+// ------ Ques 1
+
+// ---------- Part 1
+var firstName = "Muhammad Faizan";
+alert("First Name : " + firstName);
+
+// ---------- Part 2
+
+var lastName = "Memon";
+alert("Last Name : " + lastName);
+
+// ---------- Part 3
+
+var email = "Smartfaiza53@gamil.com"
+alert("Email : "  + email);
+
+// ---------- Part 4
+
+var phoneNumber = "03XX-XXXXXXX";
+alert("Phone Number : " + phoneNumber);
+
+// ---------- Part 5
+
+var password = "*********"
+alert("Password : "  + password);
+
+// ------ Ques 2
+
+alert("You 're learning javascript");
+
+// ------ Ques 3
+
+alert("no enter net plz wait");
+
+
+// ---------------CHAPTER ------ END
+
+
+// --------------- Chapter 2 (Variables for string)
+
+
+// ------ Ques 1
+
+var myVariableName = "camelcase use";
+
+// ------ Ques 2
+
+var myVariable;
+myVariable = "This is a string value.";
+
+// ------ Ques 3
+
+var teamName = "MyTeam";
+alert("Team Name: " + teamName);
+
+// ------ Ques 4
+
+var bestMan = "Charlie";
+bestMan = "David";
+
+
+// ---------------CHAPTER ------ END
+
+
+// --------------- Chapter 3 (Variables for numbers)
+
+
+// ------ Ques 1
+
+var caseQty;
+
+// ------ Ques 2
+
+var caseQty; 
+caseQty = 144; 
+
+
+// ------ Ques 3
+
+var num = 9;
+
+// ------ Ques 4
+
+var result;
+result = 8 + 5;
+
+// ------ Ques 5
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+console.log(orderTotal);
+
+// ------ Ques 6
+
+var myNum = 5; 
+myNum = myNum + 3; 
+
+console.log(myNum); 
+
+
+// ---------------CHAPTER ------ END
+
+
+// --------------- Chapter 4 (Variable names Legal and Illegal)
+
+
+// ------ Ques 1
+
+var cost = 3.45;
+
+// ------ Ques 2
+
+var nameOfBand;
+
+// ------ Ques 3
+
+var $res = "14";
+
+// ------ Ques 4
+
+var firstName = "John";
+var lastName = "Doe";
+var fullName = firstName + lastName; 
+
+// ------ Ques 5
+
+Legal Variable Names:
+
+myVar
+name
+_underscoreVar
+$dollarVar
+camelCaseVar
+firstName
+lastName
+fullName
+Illegal Variable Names:
+
+3rdPlace (Variable names cannot start with a number)
+product cost (Variable names cannot contain spaces)
+my-variable (Variable names cannot contain hyphens)
+alert (Reserved keyword in JavaScript)
+for (Reserved keyword in JavaScript)
+var (Reserved keyword in JavaScript)
+
+
+// ---------------CHAPTER ------ END
+
+
+// --------------- Chapter 5 (Math Expression I)
+
+
+
+// ------ Ques 1
+
+
+
+// ------ Ques 2
+
+20 % 6 = 2
+
+// ------ Ques 3
+
+var largeNum = 1000 * 2000;
+
+// ------ Ques 4
+
+var num1 = 13;
+var num2 = 7;
+var result = num1 - num2
+
+// ------ Ques 5
+
+var myRemainder = 20 % 8;
+
+// ------ Ques 6
+
+
+var num1 = 6;
+var num2 = 8;
+var multiplicationResult = num1 * num2;
+
+alert("The result of the multiplication is: " + multiplicationResult);
+
+
+
+//---------------------------- End -----------------------------//
